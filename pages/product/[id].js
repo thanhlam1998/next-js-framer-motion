cfa076caa1d3a9a65fa96ffea0034b276a3f3319
thanhlam <lamnguyen11e@gmail.com>
2022-01-24 12:02:46 +0700

    :lipstick: add UI without style for home and product detail

---
+++ pages/product/[id].js
@@ -0,0 +1,56 @@
+import React from "react";
+import Image from "next/image";
+import { productDetail } from "../../src/data";
+import Link from "next/link";
+
+const Product = ({ product }) => {
+  return (
+    <div>
+      <div className="fullscreen">
+        <div className="product">
+          <div className="img">
+            <Image layout="fill" src={product.image} alt={product.name} />
+          </div>
+          <div className="product-details">
+            <div className="inner">
+              <Link passHref href="/">
+                <div>
+                  <a className="go-back">Back to products</a>
+                </div>
+              </Link>
+              <div>
+                <span className="category">Protein</span>
+              </div>
+              <h1>{product.name}</h1>
+              <p>{product.details}</p>
+              <div className="additional">
+                <span>Soy Free</span>
+                <span>Gluten Free</span>
+              </div>
+              <div className="qty-price">
+                <div className="qty">
+                  <div className="minus">-</div>
+                  <div className="amount">1</div>
+                  <div className="add">+</div>
+                </div>
+                <span className="price">{product.price}</span>
+              </div>
+              <div className="btn-row">
+                <button className="add-to-cart"> Add to cart</button>
+                <button className="subscribe"> Subscribe</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Product.getInitialProps = async function (context) {
+  const { id } = context.query;
+  const product = productDetail[id];
+  return { product };
+};
+
+export default Product;
---
+++ src/data.js
@@ -0,0 +1,37 @@
+export const productsData = [
+  {
+    id: "ghost-whey-x-chips-ahoy",
+    name: "Ghost Whey X Chips Ahoy",
+    details:
+      "We've said it before and we will say it again, nothing beats the real thing. With years of R&D and REAL CHIPS AHOY!® cookie pieces in every scoop, this flavor is second to none.",
+    price: "$39.99",
+    image: "https://cdn.shopify.com/s/files/1/1312/1975/products/CleanLeanFront@2x.png?v=1630348882",
+  },
+  {
+    id: "ghost-whey-vegan",
+    name: "GHOST® Vegan Protein",
+    details:
+      "GHOST Vegan Protein combines a premium, fully disclosed vegan protein blend with industry-leading flavors...what more could you ask for?",
+    price: "$49.99",
+    image: "https://cdn.shopify.com/s/files/1/2060/6331/products/Vegan.png?v=1574882358",
+  },
+];
+
+export const productDetail = {
+  "ghost-whey-x-chips-ahoy": {
+    id: "ghost-whey-x-chips-ahoy",
+    name: "Ghost Whey X Chips Ahoy",
+    details:
+      "We've said it before and we will say it again, nothing beats the real thing. With years of R&D and REAL CHIPS AHOY!® cookie pieces in every scoop, this flavor is second to none.",
+    price: "$39.99",
+    image: "https://cdn.shopify.com/s/files/1/1312/1975/products/CleanLeanFront@2x.png?v=1630348882",
+  },
+  "ghost-whey-vegan": {
+    id: "ghost-whey-vegan",
+    name: "GHOST® Vegan Protein",
+    details:
+      "GHOST Vegan Protein combines a premium, fully disclosed vegan protein blend with industry-leading flavors...what more could you ask for?",
+    price: "$49.99",
+    image: "https://cdn.shopify.com/s/files/1/2060/6331/products/Vegan.png?v=1574882358",
+  },
+};
